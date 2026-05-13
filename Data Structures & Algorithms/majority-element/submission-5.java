0b04f7e306787b3ce1a44cc6d908509c693d58de
TARUN-TYAGII <84266872+TARUN-TYAGII@users.noreply.github.com>
2026-05-13 15:09:30 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans=0;
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        for(int num: nums){
+            int freq = map.get(num);
+            if(freq > nums.length/2){
+                ans = num;
+                break;
+            }
+        }
+        
+        return ans;
+    }
+}
