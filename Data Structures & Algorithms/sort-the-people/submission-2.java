7fb1605602d7451ee3d9adb75950d75d1c57ba31
TARUN-TYAGII <84266872+TARUN-TYAGII@users.noreply.github.com>
2026-06-14 15:21:57 +0530

    Add: sort-the-people - submission-2

---
+++ Data Structures & Algorithms/sort-the-people/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> map = new HashMap<>();
+
+        for(int i=0;i<heights.length;i++){
+            map.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+        String[] ans = new String[heights.length];
+        for(int i=0;i<heights.length;i++){
+            ans[i] = map.get(heights[heights.length - 1 -i]);
+        }
+        return ans;
+        
+    }
+}
